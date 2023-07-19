260e3c88e57063498cea6db4a84c8c857e9ac81d
Adarsha <adarshamishra33@gmail.com>
2023-07-19 22:01:01 +0545

    added explanation

--- recursion/recursive_sum.py
+++ recursion/recursive_sum.py
@@ -8,3 +8,9 @@ def summ(arr):
     
 a = [1,2,3,4,5,6,7]
 print(summ(a))    
+
+""""The process:
+First Check if the array is empty;if it is return 0
+If its not empty then save its first element in a variable.
+Then remove it from the array.
+Then recursively add the first element and pop it off everytime until the array doesnt exist."""
